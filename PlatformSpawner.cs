55d04c13d00287b3502e33df4d9dbfffd680e8af
deep-sync <35897488+deep-sync@users.noreply.github.com>
2018-12-17 00:47:20 +0530

    Add files via upload

---
+++ PlatformSpawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour {
+    public GameObject platformPrefab;
+    Vector3 pos;
+    bool gameOver;
+    float size;
+
+	// Use this for initialization
+	void Start () {
+        pos = platformPrefab.transform.position;
+        size = platformPrefab.transform.localScale.x;
+        gameOver = false;
+
+        for (int i = 0; i < 100; i++)
+        {
+            SpawnPlatform();
+        }
+        InvokeRepeating("SpawnPlatform", 2f, 0.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnPlatform()
+    {
+        if(!gameOver)
+        {
+            int rand = Random.Range(0, 4);
+            if (rand < 2)
+            {
+                SpawnX();
+            }
+            if (rand > 2)
+            {
+                SpawnZ();
+            }
+        }
+    }
+
+
+
+    void SpawnX()
+    {
+        var lastPos = pos;
+        lastPos.x += 1;
+        pos = lastPos;
+        Instantiate(platformPrefab, pos, Quaternion.identity);
+    }
+
+    void SpawnZ()
+    {
+        var lastPos = pos;
+        lastPos.z += 1;
+        pos = lastPos;
+        Instantiate(platformPrefab, lastPos, Quaternion.identity);
+
+    }
+}
---
+++ scripts/BallController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Rigidbody rbComponent;
+    bool started = false;
+    bool gameOver = false;
+
+    private void Awake()
+    {
+
+        rbComponent = GetComponent<Rigidbody>();
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!started)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rbComponent.velocity = new Vector3(speed, 0, 0);
+                started = true;
+            }
+
+        }
+        if (!Physics.Raycast(transform.position, Vector3.down, 1f))
+        {
+            gameOver = true;
+            rbComponent.velocity = new Vector3(0, -30f, 0);
+            //Camera.main.GetComponent<CameraFollower>().gameOver = true;
+           // FindObjectOfType<PlatformSpawner>().GetComponent<PlatformSpawner>().gameOver = true;
+        }
+        if (started)
+        {
+            if (Input.GetMouseButtonDown(0) && !gameOver)
+            {
+                SwitchDirection();
+            }
+        }
+
+    }
+
+    public void SwitchDirection()
+    {
+        if (rbComponent.velocity.z > 0)
+        {
+            rbComponent.velocity = new Vector3(speed, 0, 0);
+        }
+        else if (rbComponent.velocity.x > 0)
+        {
+            rbComponent.velocity = new Vector3(0, 0, speed);
+        }
+    }
+
+}
